a45a727932577a0b36e68ab0fd744ad2a3a9325e
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-25 07:00:35 +0000

    BLD: porting chapter 4 (#7)
    * BLD: porting chapter 4
    
    * BLD: porting!

--- plots.py
+++ plots.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 from utils import signed_rank, format_decimals_factory
 
@@ -89,3 +91,43 @@ def ttest_wilcoxon_plot(data, intercept_ttest, intercept_wilcoxon):
         ax.legend(fontsize="large")
 
     return fig, axarr
+
+
+def pairs_wilcoxon_plot(data, intercept_wilcoxon):
+    fig, axarr = plt.subplots(ncols=2, figsize=[18, 8])
+
+    axarr[0].scatter(np.zeros_like(data.y), data.y, color="k")
+    axarr[0].scatter(np.ones_like(data.y2), data.y2, color="k")
+    for y, y2 in zip(data.y, data.y2):
+        axarr[0].plot([0, 1], [y, y2], color="k")
+    axarr[0].set_title("Pairs")
+
+    axarr[1].scatter(np.zeros_like(data.y_sub_y2), data.y_sub_y2, color="k")
+    annotations = data.y_sub_y2.apply(format_decimals_factory())
+    for i, annot in enumerate(annotations):
+        axarr[1].annotate(annot, (0, data.y_sub_y2[i]), color="grey")
+    axarr[1].axhline(intercept_wilcoxon, color="b", label=r"$\beta_0$ (Intercept)")
+    axarr[1].set_title("$t$-test")
+    axarr[1].legend(fontsize="large")
+
+    return fig, axarr
+
+
+def dummy_coding_plot():
+    N = 20
+    data1 = np.random.multivariate_normal([0, 0], np.identity(2), N)
+    data2 = np.random.multivariate_normal([4, 4], np.identity(2), N)
+    df = pd.DataFrame(data=np.concatenate([data1, data2]), columns=["x", "y"])
+    df["dummy"] = np.concatenate([np.zeros(N), np.ones(N)])
+
+    res = smf.ols(formula="y ~ 1 + dummy", data=df).fit()
+    beta0, beta1 = res.params
+
+    fig, ax = plt.subplots(figsize=[10, 8])
+    ax.scatter(*data1.T, color="k")
+    ax.scatter(*data2.T, color="k")
+    ax.axhline(beta0, color="c", label=r"$\beta_0$ (group 1 mean)")
+    ax.plot([beta0, beta1], [beta0, beta1], color="r", label=r"$\beta_1$ (slope = difference)")
+    ax.axhline(beta1, color="b", label=r"$\beta_0 + \beta_1$ (group 2 mean)")
+    ax.legend(fontsize="large")
+    return fig, ax
