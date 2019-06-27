786b73dd09969cf80e1291c51611726c92891b7d
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-27 15:21:38 +0800

    TST: make stronger test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-set -e
-
 jupyter nbconvert --to notebook --execute tests-as-linear.ipynb
-nbdiff --ignore-output tests-as-linear.ipynb tests-as-linear.nbconvert.ipynb > diff.txt
+nbdiff --ignore-output tests-as-linear.ipynb tests-as-linear.nbconvert.ipynb > ipynb.diff
+jupyter nbconvert --to html tests-as-linear.nbconvert.ipynb
+diff index.html tests-as-linear.nbconvert.html > html.diff
 
-if [ -s diff.txt ]
+if [ -s ipynb.diff ]
 then
-   echo "Notebook not executed in order. Rerun \`tests-as-linear.ipynb\`."
-   cat diff.txt
-   rm -rf diff.txt tests-as-linear.nbconvert.ipynb  # Clean up
+   echo "Notebook not executed in order. Rerun notebook."
+   cat ipynb.diff
    exit 1
 else
-   echo "Tests passed!"
-   rm -rf diff.txt tests-as-linear.nbconvert.ipynb  # Clean up
-   exit 0
+   rm -rf ipynb.diff tests-as-linear.nbconvert.ipynb
+   echo "Notebook executed in order!"
 fi
+
+DIFFSIZE=$(cat html.diff | wc -l)
+if [ $DIFFSIZE -gt 10 ]
+then
+   echo "HTML does not match notebook. Republish."
+   cat html.diff
+   exit 1
+else
+   rm -rf html.diff tests-as-linear.nbconvert.html
+   echo "HTML matches notebook!"
+fi
+
+exit 0
