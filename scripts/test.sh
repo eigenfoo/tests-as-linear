3776e605fe9a1e5908d0bf2fe586fbb0ff7c9239
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-27 22:07:26 +0800

    Revert "TST: make stronger test"
    This reverts commit 786b73dd09969cf80e1291c51611726c92891b7d.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,29 +1,18 @@
 #!/bin/sh
 
+set -e
+
 jupyter nbconvert --to notebook --execute tests-as-linear.ipynb
-nbdiff --ignore-output tests-as-linear.ipynb tests-as-linear.nbconvert.ipynb > ipynb.diff
-jupyter nbconvert --to html tests-as-linear.nbconvert.ipynb
-diff index.html tests-as-linear.nbconvert.html > html.diff
+nbdiff --ignore-output tests-as-linear.ipynb tests-as-linear.nbconvert.ipynb > diff.txt
 
-if [ -s ipynb.diff ]
+if [ -s diff.txt ]
 then
-   echo "Notebook not executed in order. Rerun notebook."
-   cat ipynb.diff
+   echo "Notebook not executed in order. Rerun \`tests-as-linear.ipynb\`."
+   cat diff.txt
+   rm -rf diff.txt tests-as-linear.nbconvert.ipynb  # Clean up
    exit 1
 else
-   rm -rf ipynb.diff tests-as-linear.nbconvert.ipynb
-   echo "Notebook executed in order!"
+   echo "Tests passed!"
+   rm -rf diff.txt tests-as-linear.nbconvert.ipynb  # Clean up
+   exit 0
 fi
-
-DIFFSIZE=$(cat html.diff | wc -l)
-if [ $DIFFSIZE -gt 10 ]
-then
-   echo "HTML does not match notebook. Republish."
-   cat html.diff
-   exit 1
-else
-   rm -rf html.diff tests-as-linear.nbconvert.html
-   echo "HTML matches notebook!"
-fi
-
-exit 0
