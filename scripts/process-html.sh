7c8c51ec4f75f688666f1ec7210183ab5fe15778
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-28 20:26:48 +0800

    BUG: fix twitter card

--- scripts/process-html.sh
+++ scripts/process-html.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Embed Clicky web analytics.
+# Embed Clicky web analytics and Twitter card.
 LINE=9
 head -n $LINE tests-as-linear.html > index.html
 echo '<script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101159456);</script>' >> index.html
 echo '<script async src="//static.getclicky.com/js"></script>' >> index.html
+echo '' >> index.html
+echo '<meta name="twitter:card" content="summary_large_image">' >> index.html
+echo '<meta name="twitter:site" content="@_eigenfoo">' >> index.html
+echo '<meta name="twitter:creator" content="@_eigenfoo">' >> index.html
+echo '<meta name="twitter:title" content="Common statistical tests are linear models: Python port">' >> index.html
+echo '<meta name="twitter:image" content="https://eigenfoo.xyz/tests-as-linear/cheatsheets/linear_tests_cheat_sheet.png">' >> index.html
 tail -n +$LINE tests-as-linear.html >> index.html
 
 # Change title.
